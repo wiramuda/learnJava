8925490eab49b48fb99c52ff25dc9ba638f29dd5
wiramuda <masihmanusia12@gmail.com>
2022-05-10 14:51:29 +0700

    make a gamehelper class for user input

--- src/simpledotcomgame/SimpleDotComGame.java
+++ src/simpledotcomgame/SimpleDotComGame.java
@@ -6,12 +6,15 @@ public class SimpleDotComGame {
     private static int numOfGuesses;
     public static void main(String[] args) {
         SimpleDotCom dotCom = new SimpleDotCom();
+        GameHelper helper = new GameHelper();
         int randomLocation = (int) (Math.random()*5);
         int[] locationsCell = {randomLocation,randomLocation+1,randomLocation+2};
+        dotCom.setLocationsCells(locationsCell);
         boolean isAlive = true;
         while(isAlive) {
             String userQuest = helper.getUserInput("enter a number: ");
             String result = dotCom.checkYourSelf(userQuest);
+            numOfGuesses++;
             if(result.equals("kill")) {
                 isAlive = false;
                 System.out.println("you took "+numOfGuesses+ " guesses");
